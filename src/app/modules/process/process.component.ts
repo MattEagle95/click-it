3461a27951bbf2d42acd334d86dc3af4b5532d41
MattEagle95 <55457634+MattEagle95@users.noreply.github.com>
2020-12-22 00:22:19 +0100

    Update process.component.ts

--- src/app/modules/process/process.component.ts
+++ src/app/modules/process/process.component.ts
@@ -3,6 +3,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { BehaviorSubject, interval, Observable, of, pipe, Subject, Subscription, throwError, timer } from 'rxjs';
 import { delay, flatMap, repeat, takeUntil, repeatWhen, retryWhen, take, concatMap, tap, delayWhen } from 'rxjs/operators';
 import { ApiService } from 'src/app/shared/api.service';
+import { Process } from 'src/app/shared/process';
 
 @Component({
   selector: 'app-process',
